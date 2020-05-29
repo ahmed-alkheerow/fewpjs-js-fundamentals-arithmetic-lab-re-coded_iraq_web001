874ce703bfe244a6169cc4ef46afe65d7aa54f9d
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-05-29 15:27:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const num2 = 2
 const multiply = num1*num2;
 const random = Math.floor(Math.random() * 10) ;
 const mod = 14%5;
-const max =Math.max(1, 2, 3, 4, 24);
+const max =Math.max(1, 2, 3, 4, 20);
